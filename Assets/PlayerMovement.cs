1eb29665b60134a7781c370a1724a3163d2e7947
Emily Krohn <emilykrohn@csu.fullerton.edu>
2025-07-09 20:43:33 -0700

    Add Player Jumping
    If the space bar is held down, every other jump goes double the height it should go.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -5,8 +6,17 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
+    [SerializeField] float jumpAmount = 3f;
+    bool isGrounded = true;
     Vector2 movement;
     float jump = 0f;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
@@ -18,10 +28,39 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(jump);
     }
 
+    void FixedUpdate()
+    {
+        if (isGrounded)
+        {
+            if (jump != 0)
+            {
+                rb.AddForce(Vector2.up * jumpAmount);
+            }
+        }
+    }
+
     void Update()
     {
         Vector2 position = transform.position;
         position.x += movement.x * speed * Time.deltaTime;
         transform.position = position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            Debug.Log("Grounded");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+            Debug.Log("Not Grounded");
+        }
     }
 }
