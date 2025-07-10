5fe63c1f13f22452093cdf1a8b8f86ca04b16055
Emily Krohn <emilykrohn@csu.fullerton.edu>
2025-07-09 18:45:45 -0700

    Add Left and Right Player Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,10 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float speed = 5f;
     Vector2 movement;
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
         Debug.Log(movement);
+    }
+
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x += movement.x * speed * Time.deltaTime;
+        transform.position = position;
     }
 }
