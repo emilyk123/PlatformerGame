9c71653a87512184d19b3842a19ed6a53f1c18c2
Emily Krohn <emilykrohn@csu.fullerton.edu>
2025-07-15 20:31:18 -0700

    Add Flying Enemy

---
+++ Assets/FlyingEnemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    [SerializeField] float movement = 2f;
+    [SerializeField] float flyingAmplitude = 2f;
+    [SerializeField] float flyingWavelength = 0.5f;
+    private int direction = -1;
+
+    void Update()
+    {
+        transform.position += new Vector3(movement * direction * Time.deltaTime, Mathf.Sin(flyingWavelength * Time.time) * flyingAmplitude, 0f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyBorder")
+        {
+            direction *= -1;
+        }
+    }
+}
